7afac69ea750cccb9874cd6e8ff667dab4dcabef
marcio henrique <mhmuzy@embratel.com.br>
2020-11-30 13:51:49 -0300

    Finalização dos Comentários do Programa [Treinamento]

--- src/Hitss/Treinamento/Infra/IController.java
+++ src/Hitss/Treinamento/Infra/IController.java
@@ -6,12 +6,12 @@ public interface IController {
 	/// *** Declaração da Interface Controller
 	public abstract void ligar(ControleRemoto controleRemoto); /// *** Método de Ligar o Controle
 	public abstract void desligar(ControleRemoto controleRemoto); /// *** Método de Desligar o Controle
-	public abstract void abrirMenu(ControleRemoto controleRemoto);
-	public abstract void fecharMenu(ControleRemoto controleRemoto);
-	public abstract void maisVolume(ControleRemoto controleRemoto);
-	public abstract void menosVolume(ControleRemoto controleRemoto);
-	public abstract void ligarMudo(ControleRemoto controleRemoto);
-	public abstract void desligarMudo(ControleRemoto controleRemoto);
-	public abstract void play(ControleRemoto controleRemoto);
-	public abstract void pause(ControleRemoto controleRemoto);
+	public abstract void abrirMenu(ControleRemoto controleRemoto); /// *** Método de Abrir o Menu
+	public abstract void fecharMenu(ControleRemoto controleRemoto); /// *** Método de Fechar o Menu
+	public abstract void maisVolume(ControleRemoto controleRemoto); /// *** Método de Aumentar o Volume
+	public abstract void menosVolume(ControleRemoto controleRemoto); /// *** Método de Diminuir o Volume
+	public abstract void ligarMudo(ControleRemoto controleRemoto); /// *** Método de Botar no Mudo
+	public abstract void desligarMudo(ControleRemoto controleRemoto); /// *** Método de Tirar do Mudo
+	public abstract void play(ControleRemoto controleRemoto); /// *** Método de Apertar o Play
+	public abstract void pause(ControleRemoto controleRemoto); /// *** Método de Apertar o Pause
 }
--- src/Hitss/Treinamento/Repository/ControleRemotoRepository.java
+++ src/Hitss/Treinamento/Repository/ControleRemotoRepository.java
@@ -1,116 +1,127 @@
 package Hitss.Treinamento.Repository;
 
-import Hitss.Treinamento.Classes.ControleRemoto;
-import Hitss.Treinamento.Infra.IController;
+import Hitss.Treinamento.Classes.ControleRemoto; /// *** Importando
+import Hitss.Treinamento.Infra.IController; /// *** Importando
 
 public class ControleRemotoRepository implements IController {
-
+ /// *** Declaração da Classe do Repositório do Controle Remoto Implementado pela Interface Controller
 	@Override
 	public void ligar(ControleRemoto controleRemoto) {
-		controleRemoto.setLigado(true);
+		/// *** Método de Ligar
+		controleRemoto.setLigado(true); /// *** Ligado Recebe True
 	}
 
 	@Override
-	public void desligar(ControleRemoto controleRemoto) {
-		controleRemoto.setLigado(false);
-		System.out.println("");
-		System.out.println("Até Logo.");
+	public void desligar(ControleRemoto controleRemoto) { /// *** Método de Desligar
+		controleRemoto.setLigado(false); /// *** Ligado Recebe False
+		System.out.println(""); /// *** Pula 1 Linha
+		System.out.println("Até Logo."); /// *** O Programa Informa Até Logo
 	}
 
 	@Override
 	public void abrirMenu(ControleRemoto controleRemoto) {
+		/// *** Método de Abrir Menu
 		try {
-			
-		if (controleRemoto.getLigado()) {
+			/// *** Inicialização do Tratamento
+		if (controleRemoto.getLigado()) {	
+			/// *** Se o Controle Remoto estiver ligado
 			System.out.println("");
-			System.out.println("---- MENU ----");
+			/// *** Pula 1 linha
+			System.out.println("---- MENU ----"); 
+			/// *** O Enunciado Menu
 			String RespostaLigado = controleRemoto.getLigado()? "Sim" : "Não";
+			/// *** Declaração da Variável da Resposta da Ligação se o Controle Remoto estiver Ligado a mensagem vem como Sim, caso contrário vem como Não			
 			String RespostaTocando = controleRemoto.getTocando()? "Sim" : "Não";
+			/// *** Declaração da variável que define se o controle remoto está tocando Vai Vim Sim, caso contrario, Não
 			System.out.println("Está Ligado? " + RespostaLigado);
+			/// *** O Programa Informa se o Controle Remoto Está Ligado
 			System.out.println("Está Tocando? " + RespostaTocando);
+			/// *** O Programa Infirma se o Controle Remoto está tocando
 			System.out.print("Volume: " + controleRemoto.getVolume());
+			/// *** O Programa informa o Volume do Controle Remoto
 			for (int i = 0; i <= controleRemoto.getVolume(); i+=10) {
-				System.out.print("|");
+				/// *** Comando para mandar a onda de Frequência do Controle Remoto
+				System.out.print("|"); /// *** Informar a Onda de Frequencia do Controle Remoto
 			}
-		} else {
-			System.out.println("Não consegui abrir o menu");
+		} else { /// *** Se não foi aberto o Menu
+			System.out.println("Não consegui abrir o menu"); /// *** O Programa Informa quan não conseguiu abrir o menu
 		}
-		System.out.println("");
-		} catch (Exception e) {
-			System.out.println("");
-			System.out.println("Erro de processamento: " + e.getMessage());
-		}
-	}
-
-	@Override
-	public void fecharMenu(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado()) {
-			System.out.println("Fechando Menu...");
+		System.out.println(""); /// *** Pula 1 Linha
+		} catch (Exception e) { /// *** Comando do catch
+			System.out.println(""); /// *** Pula 1 Linha
+			System.out.println("Erro de processamento: " + e.getMessage()); /// *** O Programa informa a mensagem do erro
 		}
 	}
 
 	@Override
-	public void maisVolume(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado()) {
-			controleRemoto.setVolume(controleRemoto.getVolume() + 5);
-			System.out.print("Volume");
-			for (int i = 0; i <= controleRemoto.getVolume() + 50; i+=10) {
-				System.out.print("|");
+	public void fecharMenu(ControleRemoto controleRemoto) { /// *** Método de Fechar o Menu
+		if (controleRemoto.getLigado()) { /// *** Se o Controle Remoto estiver ligado
+			System.out.println("Fechando Menu..."); /// *** O Programa Informa para fechar o Menu
+		}
+	}
+
+	@Override
+	public void maisVolume(ControleRemoto controleRemoto) { /// *** Método de Aumentar o Volume
+		if (controleRemoto.getLigado()) { /// *** Se o Controle Remoto estiver Ligado
+			controleRemoto.setVolume(controleRemoto.getVolume() + 5); /// *** O Volume Aumenta 5 Pontos
+			System.out.print("Volume"); /// *** O Programa Informa o Volume
+			for (int i = 0; i <= controleRemoto.getVolume() + 50; i+=10) { /// *** Comando Para Aumentar a Onda de Frequancia
+				System.out.print("|"); /// *** Informar a Onda de Frequencia
 			}
-			System.out.println("");
-		} else {
-			System.out.println("Impossível aumentar o volume");
+			System.out.println(""); /// *** Pula 1 Linha
+		} else { /// *** Se o Controle Remoto Estiver desligado
+			System.out.println("Impossível aumentar o volume"); /// *** O Programa informa Impossível aumentar o volume
 		}
 	}
 
 	@Override
-	public void menosVolume(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado()) {
-			controleRemoto.setVolume(controleRemoto.getVolume() - 5);
-			System.out.println("Volume ||");
-			System.out.println("");
-		} else {
-			System.out.println("Impossível diminuir o volume");
+	public void menosVolume(ControleRemoto controleRemoto) { /// *** Comando para diminuir o Volume
+		if (controleRemoto.getLigado()) { /// *** Se o Controle Remoto Estiver Ligado
+			controleRemoto.setVolume(controleRemoto.getVolume() - 5); /// *** O Volume Diminui 5 pontos
+			System.out.println("Volume ||");  /// *** O Programa Informa a Frequência do Volume
+			System.out.println(""); /// *** Pula 1 Linha
+		} else { /// *** Se o Controle Remoto Estiver Desligado
+			System.out.println("Impossível diminuir o volume"); /// *** O Programa Informa que é Impossível diminuir o volume
 		}
 	}
 
 	@Override
-	public void ligarMudo(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado() && controleRemoto.getVolume() > 0) {
-			controleRemoto.setVolume(0);
-			System.out.println("Volume 0");
-			System.out.println("");
+	public void ligarMudo(ControleRemoto controleRemoto) { /// *** Método de Apertar o Mudo
+		if (controleRemoto.getLigado() && controleRemoto.getVolume() > 0) { /// *** Se o Controle Remoto Estiver Ligado e o Volume estiver acima de 0
+			controleRemoto.setVolume(0); /// *** Volume Fica no Mudo
+			System.out.println("Volume 0"); /// *** O Programa Informa o Volume no Mudo
+			System.out.println(""); /// *** Pula 1 Linha
 		}
 	}
 
 	@Override
-	public void desligarMudo(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado() && controleRemoto.getVolume() == 0) {
-			controleRemoto.setVolume(50);
-		} else {
-			System.out.println("Impossível Tirar do Mudo");
+	public void desligarMudo(ControleRemoto controleRemoto) { /// *** Método Para Tirar do Mudo
+		if (controleRemoto.getLigado() && controleRemoto.getVolume() == 0) { /// *** Se o Controle Remoto Estiver Ligado e o Volume Estiver no Mudo
+			controleRemoto.setVolume(50); /// *** Volume Recebe 50
+		} else { /// *** Caso Contrário
+			System.out.println("Impossível Tirar do Mudo"); /// *** O Programa Informa que é Impossível Tirar do Mudo
 		}
 	}
 
 	@Override
-	public void play(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado() && (!controleRemoto.getTocando())) {
-			controleRemoto.setTocando(true);
-			System.out.println("Música Tocando...");
-			System.out.println("");
-		} else {
-			System.out.println("Não consegui reproduzir");
+	public void play(ControleRemoto controleRemoto) { /// *** Método Apertar o Play
+		if (controleRemoto.getLigado() && (!controleRemoto.getTocando())) { /// *** Se o Controle Remoto Estiver Ligado e Não Tocando
+			controleRemoto.setTocando(true); /// *** Controle Remoto Começa a Tocar
+			System.out.println("Música Tocando..."); /// *** O Programa Informa que a Música está tocando
+			System.out.println(""); /// *** Pula 1 Linh
+		} else { /// *** Caso Contrário
+			System.out.println("Não consegui reproduzir"); /// *** O Programa Informa que não conseguiu reproduzir
 		}
 	}
 
 	@Override
-	public void pause(ControleRemoto controleRemoto) {
-		if (controleRemoto.getLigado() && controleRemoto.getTocando()) {
-			controleRemoto.setTocando(false);
-			System.out.println("Música Pausada...");
-			System.out.println("");
-		} else {
-			System.out.println("Não consegui pausar");
+	public void pause(ControleRemoto controleRemoto) { /// *** Método de Dar o Pause
+		if (controleRemoto.getLigado() && controleRemoto.getTocando()) { /// *** Se o Controle Remoto Estiver Ligado e Tocando
+			controleRemoto.setTocando(false); /// *** O Controle Remoto Para de Tocar
+			System.out.println("Música Pausada..."); /// *** O Programa Informa que a Música está pausada
+			System.out.println(""); /// *** Pula 1 Linha
+		} else { /// *** Caso Contrário
+			System.out.println("Não consegui pausar"); /// *** O Programa Informa que não conseguiu Pausar
 		}
 	}
 
